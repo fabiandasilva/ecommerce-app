3b69446b1094dc6877dec53a8533916b834ada53
Fabian Da Silva <fabian.da.silva@hotmail.com>
2022-10-31 23:15:02 -0300

    Add LordIcon

--- js/pages/homePage.js
+++ js/pages/homePage.js
@@ -6,7 +6,7 @@ const renderHome = () => {
   const heroSection = document.createElement('section')
   heroSection.classList.add('hero')
   heroSection.innerHTML = `
-    <h1>BATUK ORIGINS</h1>
+     
       <video class="hero__video" autoplay muted loop>
            <source src="../../assets/video/batuk.mp4" type="video/mp4">
   </video>
